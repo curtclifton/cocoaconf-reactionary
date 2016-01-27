10e2b67aeddebb147f929f81ef74a318bd3fc1bc
Curt Clifton <curt.clifton@mac.com>
2016-01-26 22:01:08 -0800

    Instantiate project template and rough in roles model

---
+++ SmartGoals/SmartGoalsModel/Roles.swift
@@ -0,0 +1,22 @@
+//
+//  Roles.swift
+//  SmartGoals
+//
+//  Created by Curt Clifton on 1/26/16.
+//  Copyright © 2016 curtclifton.net. All rights reserved.
+//
+
+import Foundation
+
+typealias Identifier: String // CCC, 1/26/2016. Need to make this a separate, smart type
+
+public struct Role {
+    let identifier: Identifier
+    var explanation: String
+    var shortName: String
+    
+    /// Indicates whether there are any goal sets associated with the Role.
+    ///
+    /// This is used to determine whether edits should to the other properties should cause the user to be prompted to create a new role or edit an existing role. If the role is not referenced, then edits can just edit the existing one without concern for unintended side effects.
+    var isReferenced: Bool
+}
---
+++ SmartGoals/SmartGoalsModel/SmartGoalsModel.h
@@ -0,0 +1,19 @@
+//
+//  SmartGoalsModel.h
+//  SmartGoalsModel
+//
+//  Created by Curt Clifton on 1/26/16.
+//  Copyright © 2016 curtclifton.net. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SmartGoalsModel.
+FOUNDATION_EXPORT double SmartGoalsModelVersionNumber;
+
+//! Project version string for SmartGoalsModel.
+FOUNDATION_EXPORT const unsigned char SmartGoalsModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartGoalsModel/PublicHeader.h>
+
+
