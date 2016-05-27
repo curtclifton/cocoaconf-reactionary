743f2999fbee59377e8e42ee881cd0cdf3c3e6d8
Curt Clifton <curt.clifton@mac.com>
2016-05-26 22:53:32 -0700

    Park on a downhill slope

--- SmartGoals/SmartGoalsModel/Signal.swift
+++ SmartGoals/SmartGoalsModel/Signal.swift
@@ -383,6 +383,7 @@ extension Signal {
 /// Expects 0 or 1 non-nil results for each fetch.
 public func itemFetchSignal<Value>(fetchRequest fetchRequest: NSFetchRequest, context: NSManagedObjectContext, transform: AnyObject -> Value?) -> Signal<Value> {
     // CCC, 5/26/2016. We could probably make the transform here detect the 1-to-0 transition and do the result wrapping.
+    #error HERE is where you're working. 
     let signal = FetchSignal<Value>(fetchRequest: fetchRequest, context: context) { (results: [AnyObject]) -> Value? in
         let typedMatches = results.flatMap { transform($0) }
         assert(typedMatches.count <= 1)
@@ -449,7 +450,6 @@ final class FetchSignal<Value>: Signal<Value> {
             || anyEntityMatches(userInfo: userInfo, key: NSDeletedObjectsKey)
             || anyEntityMatches(userInfo: userInfo, key: NSRefreshedObjectsKey)
             || anyEntityMatches(userInfo: userInfo, key: NSUpdatedObjectsKey))
-        // CCC, 5/26/2016. If we matched on a delete and we're a single item fetcher, then we need to pass an end-of-life message to our observers. Or perhaps fetchUpdate() should track when a singleton observer transitions from 1 result to 0 results?
         if matchFound {
             fetchUpdates()
         }
